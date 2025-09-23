bdb715ea5fbb8d3dcb5dbba4f5d928609becd4ed
safehehe <samhernandezhe@unal.edu.co>
2025-09-23 07:42:37 -0500

    Formato de archivos

--- design/BCD_ASM/mux.v
+++ design/BCD_ASM/mux.v
@@ -1,7 +1,10 @@
-module mux(SELECT,out);
-input SELECT;
-output reg out;
-//                              -5         3
-always @(*) out = SELECT ? 4'b1011 : 4'b0011;
+module mux (
+    SELECT,
+    out
+);
+  input SELECT;
+  output reg out;
+  //                              -5         3
+  always @(*) out = SELECT ? 4'b1011 : 4'b0011;
 
 endmodule
--- design/BCD_ASM/sum.v
+++ design/BCD_ASM/sum.v
@@ -1,8 +1,12 @@
-module sum (A,B, out);
-input [3:0] A;
-input [3:0] B;
-output reg [3:0] out;
+module sum (
+    A,
+    B,
+    out
+);
+  input [3:0] A;
+  input [3:0] B;
+  output reg [3:0] out;
 
-always @(*) out = A + B;
+  always @(*) out = A + B;
 
 endmodule
